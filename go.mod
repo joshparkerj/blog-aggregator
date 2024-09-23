f39f04bcdd4fc27029035a830dd7039220d94957
Josh Parkker <jparker@sherpas.ai>
2024-09-22 18:27:30 -0600

    add a server that has one test route

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/joshparkerj/blog-aggregator
 
 go 1.23.1
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- main.go
+++ main.go
@@ -2,9 +2,31 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load()
+	port := os.Getenv("PORT")
 	fmt.Println("this is the main. Welcome!")
-}
+	fmt.Println("now I can edit the file yay!")
+	fmt.Println(port)
 
+	// now let's do some real coding!
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/smiley", func(sponse http.ResponseWriter, quest *http.Request) {
+		sponse.Write([]byte("say cheese!"))
+	})
+
+	server := http.Server{
+		Handler: mux,
+		Addr:    fmt.Sprintf(":%v", port),
+	}
+
+	server.ListenAndServe()
+}
